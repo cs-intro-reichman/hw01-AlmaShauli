c7c0b334f2786dbf34a9e32ac59bc8704ab2ac20
AlmaShauli <alma.shauli@post.runi.ac.il>
2023-12-20 19:13:36 +0200

    Add new solutions to questions

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,25 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		// Put your code here
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+
+		int num1 = (int) ((Math.random() * (b - a) + a));
+		int num2 = (int) ((Math.random() * (b - a) + a));
+		int num3 = (int) ((Math.random() * (b - a) + a));
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+
+		int min = num1;
+		if (min > num2) {
+			min = num2;
+		}
+		if (min > num3) {
+			min = num3;
+		}
+		System.out.println("The minimal generated number was " + min);
+
 	}
 }
--- Triangle.java
+++ Triangle.java
@@ -2,9 +2,26 @@
  * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
  * This is known as the Triangle Inequality Theorem. 
  * Write a program that tests if three given integers form a triangle.
- */ 
+ */
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		// Put your code here
+		int edgeOne = Integer.parseInt(args[0]);
+		int edgeTwo = Integer.parseInt(args[1]);
+		int edgeThree = Integer.parseInt(args[2]);
+		boolean triangle;
+
+		if (((edgeOne + edgeTwo) < edgeThree) || ((edgeOne + edgeTwo) == edgeThree)) {
+			triangle = false;
+		} else if (((edgeOne + edgeThree) < edgeTwo) || ((edgeOne + edgeThree) == edgeTwo)) {
+			triangle = false;
+		} else if (((edgeThree + edgeTwo) < edgeOne) || ((edgeThree + edgeTwo) == edgeOne)) {
+			triangle = false;
+		} else {
+			triangle = true;
+		}
+
+		System.out.println(edgeOne + ", " + edgeTwo + ", " + edgeThree + ": " + triangle);
+
 	}
 }
